75be7f708f846036ae65cdfcb5d9a0d5a76f4f91
gunyongbok <qhrrjsdyd123@gmail.com>
2024-04-02 10:48:14 +0900

    feat: 생성된 URL로 리다이렉트 시키고 프론트단에서 해당 url로 접속하면 메서드 실행

---
+++ src/main/java/eat/ouath/controller/OauthController.java
@@ -0,0 +1,32 @@
+package eat.ouath.controller;
+
+import eat.ouath.OauthServerType;
+import eat.ouath.application.OauthService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/oauth")
+@RestController
+public class OauthController {
+
+    private final OauthService oauthService;
+
+    @SneakyThrows
+    @GetMapping("/{oauthServerType}")
+    ResponseEntity<Void> redirectAuthCodeRequestUrl(
+            @PathVariable OauthServerType oauthServerType,
+            HttpServletResponse response
+    ) {
+        String redirectUrl = oauthService.getAuthCodeRequestUrl(oauthServerType);
+        response.sendRedirect(redirectUrl);
+        return ResponseEntity.ok().build();
+    }
+}
+
