685049b889971bffef244a393e66b34372b31cf0
gunyongbok <qhrrjsdyd123@gmail.com>
2024-04-02 10:38:47 +0900

    feat: 인증 서버에서 Auth code를 받아오기 위한 URL 주소

---
+++ src/main/java/eat/ouath/application/OauthService.java
@@ -0,0 +1,18 @@
+package eat.ouath.application;
+
+import eat.ouath.OauthServerType;
+import eat.ouath.domain.authcode.AuthCodeRequestUrlProviderComposite;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OauthService {
+
+    private final AuthCodeRequestUrlProviderComposite authCodeRequestUrlProviderComposite;
+
+    public String getAuthCodeRequestUrl(OauthServerType oauthServerType) {
+        return authCodeRequestUrlProviderComposite.provide(oauthServerType);
+    }
+
+}
--- src/main/java/eat/ouath/domain/authcode/AuthCodeRequestUrlProvider.java
+++ src/main/java/eat/ouath/domain/authcode/AuthCodeRequestUrlProvider.java
@@ -9,5 +9,4 @@ public interface AuthCodeRequestUrlProvider {
     OauthServerType supportServer();
 
     String provide();
-
 }
