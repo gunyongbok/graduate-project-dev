6a30658e06e39b3d441927c24dc74215dd8ac8a1
gunyongbok <qhrrjsdyd123@gmail.com>
2024-04-08 19:59:30 +0900

    feat: id를 통해 유니크함이 보장되는 회원을 찾아오거나 가입되어 있지 않는 유저는 회원가입을 진행

--- src/main/java/eat/ouath/application/OauthService.java
+++ src/main/java/eat/ouath/application/OauthService.java
@@ -1,7 +1,10 @@
 package eat.ouath.application;
 
+import eat.ouath.OauthMember;
+import eat.ouath.OauthMemberRepository;
 import eat.ouath.OauthServerType;
 import eat.ouath.domain.authcode.AuthCodeRequestUrlProviderComposite;
+import eat.ouath.domain.client.OauthMemberClientComposite;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +13,20 @@ import org.springframework.stereotype.Service;
 public class OauthService {
 
     private final AuthCodeRequestUrlProviderComposite authCodeRequestUrlProviderComposite;
+    private final OauthMemberClientComposite oauthMemberClientComposite;
+    private final OauthMemberRepository oauthMemberRepository;
 
     public String getAuthCodeRequestUrl(OauthServerType oauthServerType) {
         return authCodeRequestUrlProviderComposite.provide(oauthServerType);
     }
+
+
+    public Long login(OauthServerType oauthServerType, String authCode) {
+        OauthMember oauthMember = oauthMemberClientComposite.fetch(oauthServerType, authCode);
+        OauthMember saved = oauthMemberRepository.findByOauthId(oauthMember.oauthId())
+                .orElseGet(() -> oauthMemberRepository.save(oauthMember));
+        return saved.id();
+    }
+
 
 }
